3f6bd5a9a645a6f441fc107b63114b75d98bd36e
FridaGustavsson <friiida88@gmail.com>
2022-11-09 13:29:02 +0100

    checkpoint upg

---
+++ Inlämningsuppgift.java
@@ -0,0 +1,11 @@
+public class Inlämningsuppgift {
+    public static void main(String[] args) {
+
+        System.out.println("Inlämningsuppgift 22-11-09 ");
+
+        int i = 10;
+        int j = 25;
+        int result = obj.addNumbers (i, j);
+        System.out.println("Sum of i + j = " + result);
+    }
+}
---
+++ Numbers.java
@@ -0,0 +1,6 @@
+public class Numbers {
+
+    public int addNumbers (int i, int j);
+     int addition = i + j;
+         return addition;
+}
